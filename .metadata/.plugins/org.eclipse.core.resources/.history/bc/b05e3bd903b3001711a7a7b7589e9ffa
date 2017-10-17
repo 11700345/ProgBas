0c43cb17e1a8d692ccc3218af073a07423a54bd2
11700345 <11700345@student.pxl.be>
2017-10-17 13:47:36 +0200

    oef25 added

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/bc/b05e3bd903b3001711a7a7b7589e9ffa
@@ -0,0 +1,28 @@
+package tst;
+
+import java.util.Scanner;
+
+public class tst {
+
+	public static void main(String[] args) {
+		int x =3;
+		
+		switch (x) {
+			case 1:
+				System.out.println("1");
+				break;
+			case (2):
+				System.out.println("2 of 3");
+				break;
+			case (3):
+				System.out.println("2 of 3");
+				break;
+			default:
+				System.out.println("niet 123");
+				break;
+				
+		}
+
+	}
+
+}
--- H5/src/tst/tst.java
+++ H5/src/tst/tst.java
@@ -5,24 +5,11 @@ import java.util.Scanner;
 public class tst {
 
 	public static void main(String[] args) {
-		int x =3;
-		
-		switch (x) {
-			case 1:
-				System.out.println("1");
-				break;
-			case (2):
-				System.out.println("2 of 3");
-				break;
-			case (3):
-				System.out.println("2 of 3");
-				break;
-			default:
-				System.out.println("niet 123");
-				break;
-				
-		}
+		Scanner keyboard = new Scanner(System.in);
 
+		int test = keyboard.nextInt();
+		
+		System.out.println((test / 10000));
 	}
 
 }
